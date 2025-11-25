2e4b5532d1cf08a4648bea3f01b61c7f7eb10566
J Stenhouse <james_stenhouse@hotmail.com>
2025-11-25 14:59:16 +0000

    layout

---
+++ FilmDB/wwwroot/js/characterSearch.js
@@ -0,0 +1,16 @@
+﻿document.addEventListener("DOMContentLoaded", function ()
+{
+	document.getElementById("characterSearchForm").addEventListener("submit", function (event)
+	{
+		event.preventDefault(); // Prevent full page reload
+		document.getElementById("characterTableContainer").innerHTML = "Loading data...";
+		const query = document.getElementById("characterSearchQuery").value;
+		fetch(`/Character/CharacterSearch?query=${encodeURIComponent(query)}`)
+			.then(response => response.text())
+			.then(data =>
+			{
+				document.getElementById("characterTableContainer").innerHTML = data;
+			})
+			.catch(error => console.error("Error loading characters:", error));
+	});
+});
